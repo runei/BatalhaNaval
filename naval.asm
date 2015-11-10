70c23598ba35a1f74d5c8714ddb4e0ab75e93f81
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2015-11-10 00:59:06 -0200

    Adicionado matriz de tiros colorida
    Adicionar todos os barcos.

--- naval.asm
+++ naval.asm
@@ -31,8 +31,15 @@
 	TAM_DESTROYER EQU 3
 	TAM_PATRULHA EQU 2
 	TAM_MATRIZ EQU 10
+	TAM_VET_NAVIOS EQU 5
+	
+	VET_STR_BARCOS DB OFFSET STR_PORTAAVIOES, OFFSET STR_NAVIOGUERRA, OFFSET STR_SUBMARINO, OFFSET STR_DESTROYER, OFFSET STR_PATRULHA
+	VET_SIMB_BARCOS DB SIMB_PORTAAVIOES, SIMB_NAVIOGUERRA, SIMB_SUBMARINO, SIMB_DESTROYER, SIMB_PATRULHA
+	VET_TAM_BARCOS DB TAM_PORTAAVIOES, TAM_NAVIOGUERRA, TAM_SUBMARINO, TAM_DESTROYER, TAM_PATRULHA
 	
 	MATRIZ_JOGADOR DB 100 DUP (' ')
+	MATRIZ_TIROS DB 100 DUP (254)
+	MATRIZ_TIROS_COR DB 100 DUP (02H) ; 02H = verde
 	
 .code
 
@@ -46,22 +53,35 @@
 		RET
 	ENDP
 
-	PRINT_LINHA_TABELA_EMBARC PROC		; imprime uma linha da tabela de embarcacoes
-		PUSH DX							; AX = offset primeira coluna
+	PRINT_LINHA_TABELA_EMBARC PROC				; imprime uma linha da tabela de embarcacoes
+		PUSH DX									; CX = pos vetores
 		PUSH AX
-										; BX = offset segunda coluna
-		MOV DX, AX						; CX = offset terceira coluna
+		PUSH BX
+						
+		XOR AX, AX
+		XOR DX, DX
+			
+		MOV BX, OFFSET VET_STR_BARCOS
+		ADD BX, CX
+		MOV DL, [BX]
 		CALL PRINTFS					
 		CALL PRINTTAB
-		MOV DX, BX
-		CALL ESC_CHAR					; char imprime em DX
+		
+		MOV BX, OFFSET VET_SIMB_BARCOS
+		ADD BX, CX
+		MOV DL, [BX]
+		CALL ESC_CHAR							; char imprime em DX
 		CALL PRINTTAB
-		CALL PRINTTAB
-		MOV AX, CX
-		CALL ESC_UINT16					; int imprime em AX
+		CALL PRINTTAB    
+		
+		MOV BX, OFFSET VET_TAM_BARCOS
+		ADD BX, CX
+		MOV AL, [BX]
+		CALL ESC_UINT16							; int imprime em AX
 		
 		CALL NOVALINHA
 		
+		POP BX
 		POP AX
 		POP DX
 		RET
@@ -89,30 +109,16 @@
 		PUSH BX								; BX = offset segunda coluna
 		PUSH CX								; CX = offset terceira coluna
 		
-		MOV AX, OFFSET STR_PORTAAVIOES		;imprime porta avioes
-		MOV BX, SIMB_PORTAAVIOES			
-		MOV CX, TAM_PORTAAVIOES						
-		CALL PRINT_LINHA_TABELA_EMBARC		
-		
-		MOV AX, OFFSET STR_NAVIOGUERRA		;imprime navio de guerra
-		MOV BX, SIMB_NAVIOGUERRA		
-		MOV CX, TAM_NAVIOGUERRA					
-		CALL PRINT_LINHA_TABELA_EMBARC		
-		
-		MOV AX, OFFSET STR_SUBMARINO		;imprime submarino
-		MOV BX, SIMB_SUBMARINO				
-		MOV CX, TAM_SUBMARINO					
-		CALL PRINT_LINHA_TABELA_EMBARC		
-		
-		MOV AX, OFFSET STR_DESTROYER		;imprime destroyer
-		MOV BX, SIMB_DESTROYER			
-		MOV CX, TAM_DESTROYER			
-		CALL PRINT_LINHA_TABELA_EMBARC		
-		
-		MOV AX, OFFSET STR_PATRULHA			;imprime barco patrulha
-		MOV BX, SIMB_PATRULHA					
-		MOV CX, TAM_PATRULHA					
-		CALL PRINT_LINHA_TABELA_EMBARC		
+		MOV CX, TAM_VET_NAVIOS
+		MOV BX, 0				
+		POLTE_LOOP:
+			PUSH CX
+			MOV CX, BX
+			CALL PRINT_LINHA_TABELA_EMBARC
+			INC BX
+			POP CX
+			LOOP POLTE_LOOP
+				
 		
 		POP CX
 		POP BX
@@ -156,6 +162,38 @@
 		RET
 	ENDP
 	
+	ESC_CHAR_COLORED PROC		;printa char colorido
+		PUSH AX					; BX = endereco matriz
+		PUSH BX
+		PUSH CX
+		PUSH DX
+
+		MOV AH, 09H				; tipo da int pra printa o char, mas ele não move o cursor
+		MOV AL, DL				; AL = char
+		
+		ADD BX, 100				; +100 pq 100 é o tamanho da matriz
+		MOV DX, [BX]			; isso é um mero gambito
+		
+		MOV BH, 00H				; BH = page number
+		MOV BL, DL				; BL = cor - hoje imprime sempre verde
+		MOV CX, 01H				; CL = qtde de char que vai imprimir
+		INT 10H
+		
+		MOV AH, 03				; pega a posicao do cursor DH = linha, DL = coluna
+		INT 10H
+		
+		INC DL					; incrementa a coluna do cursor
+		
+		MOV AH, 02H				; seta o cursor para a proxima posicao
+		INT 10H
+		
+		POP DX
+		POP CX
+		POP BX
+		POP AX
+		RET
+	ENDP
+	
 	PRINT_MATRIZ PROC						; imprime uma matariz com offset em BX
 		PUSH AX
 		PUSH BX
@@ -177,7 +215,7 @@
 				CALL PRINTESPACO 
 				CALL PRINTESPACO
 				MOV DX, [BX]				; BX é o endereço da posição na MEM
-				CALL ESC_CHAR
+				CALL ESC_CHAR_COLORED
 				INC BX
 				LOOP PM_LOOP2
 			CALL NOVALINHA 
@@ -215,13 +253,30 @@
 	
 	COLOCA_BARCO_HOR PROC				; coloca um barco na posicao horizontal
 		PUSH BX							; BX = posicao inicial  em que será colocado
-		PUSH CX							; CX = tamanho do barco
+		PUSH CX							; CX = pos no vetor de barcos
+		PUSH DX							
+		PUSH AX
+		
+		MOV AX, BX
+		
+		MOV BX, OFFSET VET_SIMB_BARCOS	; pega o valor a ser colocado
+		ADD BX, CX
+		MOV DL, [BX]
+		
+		MOV BX, OFFSET VET_TAM_BARCOS	; pega o tamanho do barco
+		ADD BX, CX
+		XOR CX, CX
+		MOV CL, [BX]
+		
+		MOV BX, AX		
 		
 		CBH_LOOP1:
-			MOV [BX], SIMB_PORTAAVIOES
+			MOV [BX], Dl
 			INC BX
 			LOOP CBH_LOOP1
 			
+		POP AX
+		POP DX
 		POP CX
 		POP BX
 		RET
@@ -229,13 +284,30 @@
 	
 	COLOCA_BARCO_VERT PROC				; coloca um barco na posicao vertical
 		PUSH BX							; BX = posicao inicial  em que será colocado
-		PUSH CX							; CX = tamanho do barco
+		PUSH CX							; CX = pos no vetor de barcos
+		PUSH DX							
+		PUSH AX
+		
+		MOV AX, BX
+		
+		MOV BX, OFFSET VET_SIMB_BARCOS	; pega o valor a ser colocado
+		ADD BX, CX
+		MOV DL, [BX]
+		
+		MOV BX, OFFSET VET_TAM_BARCOS	; pega o tamanho do barco
+		ADD BX, CX
+		XOR CX, CX
+		MOV CL, [BX]
+		
+		MOV BX, AX
 		
 		CBV_LOOP1:
-			MOV [BX], SIMB_PORTAAVIOES
+			MOV [BX], DL
 			ADD BX, TAM_MATRIZ
 			LOOP CBV_LOOP1
-			
+		
+		POP AX
+		POP DX	
 		POP CX
 		POP BX
 		RET
@@ -297,36 +369,36 @@
     	JMP POSICAO_INVALIDA
     	
     	VER_POS_HOR:
-    	; horizontal , soma tamanho - 1 na posicao 
-    	; tem que ser menor que linha * 10 + 9
-    	
-    	XOR AX, AX
-    	XOR BH, BH
-    	MOV AX, BX
-    	MUL DH
-    	ADD AX, 9
-    	MOV BX, AX
-    	
-    	POP AX ; recupera posicao original
-    	ADD AX, CX
-    	
-    	CMP AX, BX
-    	
-    	JLE POSICAO_VALIDA
-    	JMP POSICAO_INVALIDA
+			; horizontal , soma tamanho - 1 na posicao 
+			; tem que ser menor que linha * 10 + 9
+			
+			XOR AX, AX
+			XOR BH, BH
+			MOV AX, BX
+			MUL DH
+			ADD AX, 9
+			MOV BX, AX
+			
+			POP AX ; recupera posicao original
+			ADD AX, CX
+			
+			CMP AX, BX
+			
+			JLE POSICAO_VALIDA
+			JMP POSICAO_INVALIDA
     	
     	POSICAO_VALIDA:
-    	MOV AX, 1
-    	JMP FIM_VER_POSICAO
+			MOV AX, 1
+			JMP FIM_VER_POSICAO
     	
     	POSICAO_INVALIDA:
-    	MOV AX, 0    
+			MOV AX, 0    
     	
     	FIM_VER_POSICAO:
     		
-    	POP BX
-    	POP CX
-    	POP DX    
+			POP BX
+			POP CX
+			POP DX    
     	
     	RET
     ENDP
@@ -337,9 +409,13 @@
 		PUSH BX
 	    PUSH CX
 	    
+		XOR DX, DX
+		
 	    MOV DX, OFFSET STR_DIGITE_POSICAO	; imprime a mensagem
 		CALL PRINTFS
-		MOV DX, BX
+		MOV BX, OFFSET VET_STR_BARCOS
+		ADD BX, CX
+		MOV DL, [BX]
 		CALL PRINTFS
 		MOV DL, ':'
 		CALL ESC_CHAR
@@ -347,32 +423,31 @@
 		
 		LACO_LER_BARCO:	
 		
-		CALL GET_POSICAO_INI_BARCO			; o usuario digita a posicao do barco		
-		MOV BX, OFFSET MATRIZ_JOGADOR		; pega o offset da matriz
-		ADD BX, AX							; e adiciona a posicao digitada
-		CALL LER_DIRECAO_BARCO				; le a direcao do barco (somente H ou V)
-		
-		CALL VER_POS_VALIDA                 ; recebe DL, direcao, CX, tamanho barco e AX, posicao
-		                                    ; retorna em AX, 1 valido, 0 invalido
-		CMP AX, 1
-		JZ  CONT_COLOCA_BARCO
-		
-		CALL NOVALINHA
-		MOV DX, OFFSET STR_DIGITE_NOVAMENTE
-		CALL PRINTFS
-		CALL NOVALINHA
-		JMP LACO_LER_BARCO
-		
+			CALL GET_POSICAO_INI_BARCO		; o usuario digita a posicao do barco		
+			MOV BX, OFFSET MATRIZ_JOGADOR	; pega o offset da matriz
+			ADD BX, AX						; e adiciona a posicao digitada
+			CALL LER_DIRECAO_BARCO			; le a direcao do barco (somente H ou V)
+			
+			CALL VER_POS_VALIDA             ; recebe DL, direcao, CX, tamanho barco e AX, posicao
+											; retorna em AX, 1 valido, 0 invalido
+			CMP AX, 1
+			JZ  CONT_COLOCA_BARCO
+			
+			CALL NOVALINHA
+			MOV DX, OFFSET STR_DIGITE_NOVAMENTE
+			CALL PRINTFS
+			CALL NOVALINHA
+			JMP LACO_LER_BARCO
 		
 		CONT_COLOCA_BARCO:
-		CMP DL, CHR_HORIZONTAL
-		JZ CB_HOR
-	    CALL COLOCA_BARCO_VERT
-		JMP CB_FIM
-		CB_HOR:
-	    	CALL COLOCA_BARCO_HOR
-	    CB_FIM:
-			CALL NOVALINHA
+			CMP DL, CHR_HORIZONTAL
+			JZ CB_HOR
+			CALL COLOCA_BARCO_VERT
+			JMP CB_FIM
+			CB_HOR:
+				CALL COLOCA_BARCO_HOR
+			CB_FIM:
+				CALL NOVALINHA
 		
 		POP CX
 		POP BX
@@ -384,6 +459,11 @@
 	ATUALIZA_TELA_INI PROC
 		PUSH BX
 		
+		CALL CLEAR
+		
+		CALL PRINT_TABELA_EMBARC		; printa tabela com os dados de cada embarcacao
+		CALL NOVALINHA
+		
 		MOV BX, OFFSET MATRIZ_JOGADOR
 		CALL NOVALINHA
 		CALL PRINT_MATRIZ
@@ -392,9 +472,27 @@
 		RET
 	ENDP
 	
+	CLEAR PROC
+		PUSH AX
+		PUSH BX
+		PUSH CX
+		PUSH DX
+		
+		MOV AX,0600H    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
+		MOV BH,07H    	;ATTRIBUTE BACKGROUND AND FOREGROUND COLOR (0 - BLACK, 7 - GRAY)
+		MOV CX,0000H    ;STARTING COORDINATES
+		MOV DX, 0FFFFH  ;ENDING COORDINATES
+		INT 10H        	;FOR VIDEO DISPLAY
+		
+		POP DX
+		POP CX
+		POP BX
+		POP AX
+		RET
+	ENDP
+	
 	COLOCA_BARCO_ATUALIZA_TELA PROC		; coloca um barco na matriz e depois atualiza a tela		
 		CALL COLOCA_BARCO				
-		
 		CALL ATUALIZA_TELA_INI
 		
 		RET
@@ -402,18 +500,31 @@
 	
 	TELA_INICIAL PROC					; mostra a tela inicial do jogo
 		PUSH BX
-		PUSH CX
-		
-		CALL PRINT_TABELA_EMBARC		; printa tabela com os dados de cada embarcacao
-		CALL NOVALINHA
+		PUSH CX	
 			
 		CALL ATUALIZA_TELA_INI
 		
-		MOV BX, OFFSET STR_PORTAAVIOES	; coloca os porta avioes
-		MOV CX, TAM_PORTAAVIOES
-		CALL COLOCA_BARCO_ATUALIZA_TELA
+		MOV CX, TAM_VET_NAVIOS
+		MOV BX, 0				
+		TI_LOOP:
+			PUSH CX
+			MOV CX, BX
+			CALL COLOCA_BARCO_ATUALIZA_TELA
+			INC BX
+			POP CX
+			LOOP TI_LOOP
 		
 		POP CX
+		POP BX
+		RET
+	ENDP
+	
+	TELA_JOGO PROC
+		PUSH BX
+		
+		MOV BX, OFFSET MATRIZ_TIROS
+		CALL PRINT_MATRIZ
+		
 		POP BX
 		RET
 	ENDP
@@ -422,7 +533,12 @@
 		MOV AX, @DATA
 		MOV DS, AX
 		
+		CALL CLEAR
 		CALL TELA_INICIAL
+		
+		CALL CLEAR
+		CALL TELA_JOGO
+		
 		
 		MOV AH, 04CH
 		INT 21H		
